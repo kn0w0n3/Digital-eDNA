848cf1bd24ea3ab80fb50bad00e52cd2a997a8fb
kn0w0n3 <tr14rc3@gmail.com>
2023-08-13 23:50:02 -0700

    Add RecordData | MapsActivity

--- DigitaleDNA/app/src/main/java/com/secretcitylabs/digitaledna/MainController.java
+++ DigitaleDNA/app/src/main/java/com/secretcitylabs/digitaledna/MainController.java
@@ -2,14 +2,28 @@ package com.secretcitylabs.digitaledna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainController extends AppCompatActivity {
+    Button recordDataBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_controller);
         getSupportActionBar().hide();
+
+        //Open the input data activity when user clicks button
+        recordDataBtn = findViewById(R.id.inputDataBtn);
+        recordDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainController.this, RecordData.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ DigitaleDNA/app/src/main/java/com/secretcitylabs/digitaledna/RecordData.java
@@ -0,0 +1,14 @@
+package com.secretcitylabs.digitaledna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_data);
+    }
+}
